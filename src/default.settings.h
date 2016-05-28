4b14f3f37c087afd1671b338c02a8f8c0843a8b5
zaporylie <jakub@nymedia.no>
2016-05-28 10:26:53 +0200

    index

--- src/default.settings.h
+++
@@ -1,9 +0,0 @@
-#define HOSTNAME "temperature"
-#define REPOSITORY "espstack/ds18b20"
-
-#define INTERVAL 10000
-#define ONE_WIRE_BUS 0
-
-#define PHANT_HOST "data.sparkfun.com"
-#define PHANT_STREAMID "test1"
-#define PHANT_PRIVATEKEY "test2"
